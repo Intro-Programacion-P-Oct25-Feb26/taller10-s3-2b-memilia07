dedbff1b84ff07789d6cdf0b59f5cdf9cb85581a
mesolano9 <mariaemiliasolano43@gmail.com>
2025-12-18 11:06:33 -0500

    problema01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,42 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}};
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+        int[] diasAsistidos = new int[estudiantes.length];
+
+        for (int fila = 0; fila < asistencia.length; fila++) {
+            int contador = 0;
+            for (int col = 0; col < asistencia[fila].length; col++) {
+                if (asistencia[fila][col]) {
+                    contador = contador + 1;
+
+                }
+
+            }
+
+            diasAsistidos[fila] = contador;
+
+        }
+
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            System.out.printf("Estudiantes: %s\nDias asistidos: %d ",
+                    estudiantes[fila], diasAsistidos[fila]);
+
+            if (diasAsistidos[fila] == 5) {
+                System.out.println("(Cumple su asistencia)");
+
+            } else {
+                System.out.println("(No cumple su asistencia)");
+
+            }
+
+        }
+
     }
-    
 }
--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,15 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}};
+        
+        
+
+    }
 }
