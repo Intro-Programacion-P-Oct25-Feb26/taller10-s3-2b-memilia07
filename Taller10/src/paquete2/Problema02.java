eb3278f57b891a0e6f8270b774e9f196f18eb765
mesolano9 <mariaemiliasolano43@gmail.com>
2025-12-18 11:19:21 -0500

    problema02

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -17,8 +17,27 @@ public class Problema02 {
             {5, 12, 35, 40},
             {8, 15, 33, 38},
             {6, 10, 30, 36}};
-        
-        
 
+        int menores = 0;
+        int[] adultosF = new int[edades.length];
+
+        for (int fila = 0; fila < edades.length; fila++) {
+            int adultos = 0;
+            for (int col = 0; col < edades[fila].length; col++) {
+                if (edades[fila][col] < 18) {
+                    menores = menores + 1;
+                } else {
+                    adultos = adultos + 1;
+                }
+            }
+
+            adultosF[fila] = adultos;
+        }
+
+        System.out.printf("Total de menores de edad: %d\n", menores);
+
+        for (int fila = 0; fila < adultosF.length; fila++) {
+            System.out.printf("Familia %d: %d adultos\n", (fila + 1), adultosF[fila]);
+        }
     }
 }
